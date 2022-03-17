3fee45a74e0f21a1c0a3d2114881f465f3b30ee3
xiaoliangtang <1290086217@qq.com>
2022-03-17 17:52:27 +0800

    提交

---
+++ java-base/src/main/java/com/java/learning/base/collection/set/LearnHeap.java
@@ -0,0 +1,33 @@
+package com.java.learning.base.collection.set;
+
+class Person {
+
+    int age;
+
+}
+
+
+public class LearnHeap {
+
+
+    public static void main(String[] args){
+
+        int a=10;
+        Person person = new Person();
+        person.age =20;
+
+        change(a,person);
+        System.out.println("a=" + a + ",and person.age = " + person.age);
+
+    }
+
+    static void change(int a1, Person person){
+
+        a1 = 11;
+        person.age= 21;
+
+        System.out.println("a1="+ a1+",and age1 = "+person.age);
+
+    }
+
+}
--- java-base/src/main/java/com/java/learning/base/collection/set/LinkedHashSetTest.java
+++ java-base/src/main/java/com/java/learning/base/collection/set/LinkedHashSetTest.java
@@ -9,11 +9,29 @@ public class LinkedHashSetTest {
     }
 
     static void test() {
-        LinkedHashSet<String> set = new LinkedHashSet<>();
-        set.add("abc");
-        set.add("efg");
-        set.add("abb");
+//        LinkedHashSet<String> set = new LinkedHashSet<>();
+//        set.add("abc");
+//        set.add("efg");
+//        set.add("abb");
+//
+//        System.out.println(set);
 
-        System.out.println(set);
+
+        String s1 = new String("abcd");
+        String s2 = "asdfghjkl";
+        int[] num = new int[] {1, 2, 3};
+        System.out.println(s1+", "+s2);
+        change(s1,s2, num);
+        System.out.println(s1+", "+s2 + ", " + num[0]);
+
     }
+
+    static void change(String s1,String s2, int[] num){
+        s1 =new String("12345");
+        s2 ="000000";
+        num[0] = 10;
+        System.out.println(s1+", "+s2 + ", " + num[0]);
+
+    }
+
 }
